c5ac8ac32cd93ab98a4b85f5c18a5a854684153e
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-04-16 19:52:15 -0600

    Update contact.jsx

--- portfolio/src/components/sections/contact.jsx
+++ portfolio/src/components/sections/contact.jsx
@@ -1,12 +1,17 @@
-export const contact = () => {
+import {RevealOnScroll}
 
-    return <section id="contact">
+export const Contact = () => {
+
+    return <section id="contact" className="min-h-screen flex items-center justify-center py-20">
+
+
+        <Reve
 
         <div> 
 
             <h2> My contacts </h2>
 
-            
+
         </div>
 
     </section>
