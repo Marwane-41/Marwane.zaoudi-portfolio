6e6f95f26393cc6e658e606164bde7da91aeac13
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-04-16 19:43:08 -0600

    Update home.jsx

--- portfolio/src/components/sections/home.jsx
+++ portfolio/src/components/sections/home.jsx
@@ -19,8 +19,9 @@ export const Home = () => {
                     View projects
                 </a>
 
-                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 
-                hover:shadow-[0_0_15px-rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"> 
+                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10
+             motion-reduce:transform-none motion-reduce:shadow-none"> 
                     Contact me 
                 </a>
                 <button>
