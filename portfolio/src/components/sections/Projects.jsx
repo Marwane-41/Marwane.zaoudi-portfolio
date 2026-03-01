c1e8128481feb30371ade6dce4f60676484f9cec
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2026-03-01 14:11:12 +0000

    added new project

--- portfolio/src/components/sections/Projects.jsx
+++ portfolio/src/components/sections/Projects.jsx
@@ -12,11 +12,35 @@ return <section id = "projects" className="min-h-screen flex items-center justif
         </h2>
 
 
-        
-
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
-        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
+                <h3 className="text-xl font-bold mb-1 "> LandlordHub </h3>
+                <p className="text-gray-400 mb-4"> A MERN full stack property management web application. </p>
+
+                <div className="flex flex-wrap gap-2"> 
+                    {["REACT", " NODE ", "MONGODB", "EXPRESS"].map((tech,key)=> (
+                        <span key={key}
+                        className="bg-blue-500/10 text-blue-500 py-1 px-4 rounded-full text-md hover:bg-blue-500/20 
+                         hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all ">
+                        {tech}
+                        </span>
+                    ))}                
+                </div>
+
+                <div className="flex flex-wrap gap-2 mb-4"> 
+                    <a href="https://github.com/Marwane-41/Fresh-Start"
+                        target= "_blank"
+                    className="text-blue-400 hover:text-blue-300 transition-colors my-4">  View Project  </a>
+                </div>
+
+            </div>
+
+
+
+
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1) transition-all">
                 <h3 className="text-xl font-bold mb-1 "> Fresh Start </h3>
                 <p className="text-gray-400 mb-4"> An ongoing freelance initiative I founded to help small, underrepresented local businesses create or modernize their online presence </p>
 
