b10376c1c27e0fd81b5b97b50c73e3b31f4d89ca
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2026-03-01 14:15:02 +0000

    few updates

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css'
+
 import { LoadingScreen } from './components/loadingScreen';
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
